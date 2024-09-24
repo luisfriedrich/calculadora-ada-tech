f61e589f7a7f7f13cb1946603f2695cbb920d2d1
Felipe <luisfriedrich7@gmail.com>
2024-09-23 22:06:23 -0300

    implementar layout com bottoes

--- script.js
+++ script.js
@@ -1,1 +1,16 @@
-console.log('==== Oi Louis ====')
+function concatenarAoResultado(value) {
+    document.getElementById('result').value += value;
+}
+
+function limparResultado() {
+    document.getElementById('result').value = '';
+}
+
+function calcularResultado() {
+    const resultField = document.getElementById('result');
+    try {
+        resultField.value = eval(resultField.value);
+    } catch (error) {
+        resultField.value = 'Erro';
+    }
+}
